0ede0ce44e3f036a1648f7c0e23f0da74587d4ca
J White <jenessaw31@gmail.com>
2021-10-11 08:55:37 -0500

    remove our story

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,10 @@
-import { useEffect, useRef } from "react";
-const LINK_CLASS = "m-2 p-2 border-b border-b-gold font-semibold text-white relative inline-block overflow-hidden transition duration-200"
-export default function Header({ title }) {
-  const btn = useRef()
-  const menu = useRef()
+import { useState } from "react";
+const LINK_CLASS = "m-2 p-2 border-b border-gold font-semibold text-white relative inline-block overflow-hidden transition duration-200"
+const MOBILE_LINK_CLASS = "mx-3 px-1 py-3 border-b border-gold font-semibold text-white"
+export default function Header() {
+  const [toggleMobileMenu, setToggleMobileMenu] = useState(false);
+  const toggle = () => setToggleMobileMenu(!toggleMobileMenu);
 
-  useEffect(() => {
-    // btn?.addEventListener("click", () => {
-    //   menu.classList.toggle("hidden");
-    // });
-    // return () => (btn?.addEventListener("click", () => {
-    //   menu.classList.toggle("hidden");
-    // }))
-  });
   return (
     <nav className="bg-black shadow-lg sticky z-10 w-full" style={{ top: '-1px' }}>
       <div className="w-full max-w-7xl mx-auto px-4">
@@ -25,7 +18,6 @@ export default function Header({ title }) {
 
           {/* <!-- Primary Navbar items --> */}
           <div className="nav hidden md:flex items-center">
-            <a className={LINK_CLASS} href="/our-story">Our Story</a>
             <a className={LINK_CLASS} href="/schedule">Schedule</a>
             <a className={LINK_CLASS} href="/travel">Travel</a>
             <a className={LINK_CLASS} href="/registry">Registry</a>
@@ -34,8 +26,8 @@ export default function Header({ title }) {
           </div>
 
           {/* <!-- Mobile menu button --> */}
-          <div className="md:hidden flex items-center">
-            <button className="outline-none mobile-menu-button" ref={btn}>
+          <div className="md:hidden flex items-center p-4">
+            <button className="outline-none mobile-menu-button" onClick={toggle}>
               <svg className=" w-6 h-6 text-gray-500 hover:text-green-500 "
                 x-show="!showMenu"
                 fill="none"
@@ -53,13 +45,12 @@ export default function Header({ title }) {
 
 
         {/* <!-- mobile menu --> */}
-        <div className="hidden mobile-menu" ref={menu}>
-          <ul className="">
-            <li className="active"><a href="index.html" className="block text-sm px-2 py-4 text-white bg-green-500 font-semibold">Home</a></li>
-            <li><a href="#services" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Services</a></li>
-            <li><a href="#about" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">About</a></li>
-            <li><a href="#contact" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Contact Us</a></li>
-          </ul>
+        <div className={`${!toggleMobileMenu && 'hidden'} slide-down md:hidden flex flex-col pb-5`}>
+          <a className={MOBILE_LINK_CLASS} href="/schedule">Schedule</a>
+          <a className={MOBILE_LINK_CLASS} href="/travel">Travel</a>
+          <a className={MOBILE_LINK_CLASS} href="/registry">Registry</a>
+          <a className={MOBILE_LINK_CLASS} href="/faqs">FAQs</a>
+          <a className={MOBILE_LINK_CLASS} href="/rsvp">RSVP</a>
         </div>
       </div>
     </nav>
