dd6f2a6b7c4a99e850389e8a8f482a2d53d57542
J White <jenessaw31@gmail.com>
2021-10-16 23:41:55 -0500

    shipt it

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,76 @@
 export default function Home() {
   return (
     <div className="w-full overflow-x-hidden">
-      <div class="parallax relative bg-fixed bg-bottom bg-no-repeat bg-cover opacity-70">
+      <div class="parallax relative bg-fixed bg-no-repeat bg-cover opacity-70">
         <div class="caption w-full text-center">
-          <h1 class="font-semibold text-6xl text-white" style={{ textShadow: "4px 1px 10px black" }}>We're getting married!</h1>
+          <div className="font-semibold text-white flex flex-col bg-opacity-70 rounded p-2 mx-auto max-w-xs sm:bg-black md:bg-opacity-0 md:max-w-none md:w-4/5">
+            <h1 class="font-semibold text-5xl text-white" style={{ textShadow: "3px 1px 10px rgba(0,0,0,0.8)" }}>We're getting married!</h1>
+          </div>
         </div>
       </div>
 
       <div className="w-full p-3 md:py-5 md:px-5">
         <div className="flex-col max-w-2xl mx-auto items-center justify-center text-center">
           <div className="md:my-6">
-            <h2 className="text-4xl">The Beginning</h2>
-            <h3 className="font-semibold my-2">- 02.05.19 -</h3>
+            <h2 className="text-4xl">WEDDING DAY</h2>
+            <h3 className="font-semibold mb-2">- 12.31.21 -</h3>
             <p>
-              We met in one of our favorite places, the gym. During the 2019 Crossfit Open, J, who has an annoying but adorable habit of being very supportive, was cheering for every class on Fridays in February during the competition. Sasha, who was attending the early sessions to AVOID the large late night classes, was simply minding her business. J kept coming and cheering and Sasha kept working out and trying to ignore J in the corner. Eventually Sasha started to come to later classes to cheer for J, and their other mutual friends (though I'm pretty sure Sasha was solely coming to cheer for me, J 🙃)
+              5:30PM – 12:30 AM
+              <br />
+              Watson Block
+              <br />
+              125 SE Main St, Minneapolis, MN, 55414
+              <br />
+              Attire: Formal Dress Requested
             </p>
           </div>
         </div>
 
-        <hr className="mx-auto w-3/4 my-12 md:w-48 md:my-20" />
+        <hr className="my-6 md:my-10 md:mx-12" />
+        <div className="flex-col max-w-2xl mx-auto items-center justify-center text-center">
+          <div className="md:my-6">
+            <h2 className="text-4xl">The Beginning</h2>
+            <h3 className="font-semibold mb-2">- 02.05.19 -</h3>
+            <p>
+              We met in one of our favorite places, the gym. During the 2019 Crossfit Open, J, who has an annoying but adorable habit of being very supportive, was cheering for every class on Fridays in February during the competition. Sasha, who was attending the early sessions to AVOID the large late night classes, was simply minding her business.
+            </p>
+            <p>
+              J kept coming and cheering and Sasha kept working out and trying to ignore J in the corner. Eventually Sasha started to come to later classes to cheer for J, and their other mutual friends (though I'm pretty sure Sasha was solely coming to cheer for me, J 🙃)
+            </p>
+            <p>
+              We went on our first date shortly after the Open was over. We easily could have talked for hours, but eventually ended a bit awkwardly when J attempted to buckle Sasha into her truck. J quickly redeemed herself on another date, and then another.
+            </p>
+          </div>
+        </div>
+
+        <hr className="my-6 md:my-10 md:mx-12" />
 
         <div className="flex-col max-w-2xl mx-auto items-center justify-center text-center">
           <div className="md:my-6">
             <h2 className="text-4xl">The Proposal</h2>
-            <h3 className="font-semibold my-2">- 07.17.20 -</h3>
+            <h3 className="font-semibold mb-2">- 07.19.20 -</h3>
+
+            <img src="/images/hero.jpeg" alt="" className="rounded my-3 md:my-6" />
             <p>
-              I woke up one day and knew it was time to propose. There's nobody I'd rather spend my time with in the world than Sasha. So I began plotting. I knew I wanted to involve some of the people we love most in the world, but not do it 'in public'. I came up with the idea for the cards just when we were playing Phase10 one night, we played that game A LOT. I lose to Sasha A LOT, so it was perfect.
+              I woke up one day and knew it was time to propose. There's nobody I'd rather spend my time with in the world than Sasha. So I began plotting. I knew I wanted to involve some of the people who we love most in the world, but not do it 'in public'. I came up with the idea for the cards just when we were playing Phase10 one night, we played that game A LOT. I lose to Sasha A LOT, so it was perfect.
             </p>
             <p>
-              Sasha knew I was proposing probably for weeks, but for sure the day of & before. I can't keep a secret from her. I wanted to just tell her I was doing it so she knew! Which isn't the point but oh well.
+              Sasha knew I was proposing probably for weeks, but for sure the day of & before. I can't keep a secret from her. I wanted to just tell her I was doing it so she knew! Which isn't the point but oh well. I didn't tell her and our friends and family actually kept the secret. Sasha has some spidey senses but went along with it!
             </p>
             <p>
-              I was so nervous, everybody that was there could probably tell. My hands were shaking, I was sweating so much,
+              I was so nervous, everybody that was there could probably tell. My hands were shaking, I was sweating so much. But once she was in front of me grinning I knew it was exactly what I was supposed to be doing and who I was supposed to be doing it with.
             </p>
           </div>
-          <img src="/images/proposal.jpeg" alt="" className="" />
         </div>
 
-        <div class="parallax relative bg-fixed bg-bottom bg-no-repeat bg-cover opacity-70">
-          <div class="caption w-full text-center">
-            <div class="font-semibold text-6xl text-white" style={{ textShadow: "4px 1px 10px black" }}>We're getting married!</div>
-            <a href="/rsvp" className="border p-3 font-semibold">RSVP</a>
+      </div>
+      <div className="bg-cover bg-fixed bg-no-repeat flex opacity-70 parallax-bottom relative md:mt-10">
+        <div className="caption-bottom text-center">
+          <div className="font-semibold text-white flex flex-col bg-black bg-opacity-70 rounded p-4 mx-auto max-w-xs">
+            <p style={{ textShadow: "3px 1px 10px rgba(0,0,0,0.8)" }}>
+              We look forward to sharing this moment with you and bringing in a new year together.
+            </p>
+            <a href="/rsvp" className="px-3 py-2 font-medium my-2 rounded-sm border border-black text-black bg-gold transition-colors duration-300 hover:text-gold hover:bg-black">RSVP</a>
           </div>
         </div>
       </div>
